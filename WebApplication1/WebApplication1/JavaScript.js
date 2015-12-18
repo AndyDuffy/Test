524cf0e60b2588496c636e17d8f419a09bbef17f
AndyDuffy <andyduffy5@gmail.com>
2015-12-18 14:18:54 +0000

    Pomodoara Clock, buttons don't work

---
+++ WebApplication1/WebApplication1/JavaScript.js
@@ -0,0 +1,34 @@
+﻿breakTot = 5;
+sessionTot = 25;
+
+$('#breakLess').on('click', function (e) {
+    if (breakTot > 0) {
+        breakTot -= 1;
+        document.getElementById("myBreak").innerHTML = breakTot;
+    }
+});
+
+$('#breakMore').on('click', function (e) {
+    breakTot += 1;
+    document.getElementById("myBreak").innerHTML = breakTot;
+});
+
+$('#sessionLess').on('click', function (e) {
+    if (sessionTot > 0) {
+        sessionTot -= 1;
+        document.getElementById("mySession").innerHTML = sessionTot;
+        document.getElementById("minutes").innerHTML = sessionTot;
+    }
+
+});
+$('#sessionMore').on('click', function (e) {
+
+    sessionTot += 1;
+    document.getElementById("mySession").innerHTML = sessionTot;
+    document.getElementById("minutes").innerHTML = sessionTot;
+});
+$('#startClock').on('click', function (e) {
+    var countSec = setInterval(mySec, 1000);
+    document.getElementById("myBreak").innerHTML = breakTot;
+}
+);
